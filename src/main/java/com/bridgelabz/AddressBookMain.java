835384106ada1f2cdb06aabb00672182189b8ce3
datta1808 <bagaledatta18@gmail.com>
2021-05-28 01:15:12 +0530

    ADDED: Functionality to add multiple contacts to address book

--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,31 +1,11 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
+import java.lang.*;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Address Book");
+        System.out.println("Welcome to Address Book System ");
         AddressBook addressBook = new AddressBook();
-        addressBook.addDetails();
-
-        System.out.println("Address Book details:");
-        System.out.println(addressBook.person);
-
-        System.out.println("Enter the choice \n 1. Edit \n 2. Delete");
-        int op = sc.nextInt();
-
-        switch (op){
-            case 1:
-                addressBook.editContact();
-                System.out.println("You have Entered following data");
-                System.out.println(addressBook.person);
-                System.out.println("Thank you for Using the Address book");
-                break;
-            case 2:
-                addressBook.delete();
-                System.out.println("Addess Book details :"+addressBook.person);
-                break;
-        }
+        addressBook.getMenu();
     }
 }
