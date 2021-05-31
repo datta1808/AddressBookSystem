d8c88e873ea1f1b6ea6eddc4946f69c9be019d71
datta1808 <bagaledatta18@gmail.com>
2021-06-01 02:04:50 +0530

    ADDED: Functionality to ensure no Duplicate entries for the same Person

--- src/main/java/com/bridgelabz/Contact.java
+++ src/main/java/com/bridgelabz/Contact.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 public class Contact {
+
     private String firstName;
     private String lastName;
     private String address;
@@ -10,7 +11,7 @@ public class Contact {
     private String phoneNumber;
     private String email;
 
-    Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+    public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -24,89 +25,69 @@ public class Contact {
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public String getCity() {
         return city;
     }
-
     public void setCity(String city) {
         this.city = city;
     }
-
     public String getState() {
         return state;
     }
-
     public void setState(String state) {
         this.state = state;
     }
-
     public String getZip() {
         return zip;
     }
-
     public void setZip(String zip) {
         this.zip = zip;
     }
-
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public boolean equals(Object object) {
+
+    @Override
+    public boolean equals(Object obj) {
         boolean result = false;
-        if(object == this) {
+
+        if(obj == this) {
             return true;
         }
-        Contact contact = (Contact)object;
+        Contact contact = (Contact)obj; //casted the above object inside Contact
         if(contact.firstName.equals(this.firstName) && contact.lastName.equals(this.lastName)) {
             result = true;
         }
         return result;
     }
+
     @Override
     public String toString() {
-        return "Contact{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zip=" + zip +
-                ", phoneNumber=" + phoneNumber +
-                ", email='" + email + '\'' +
-                '}';
+        return this.getFirstName() + " " + this.getLastName() + " " + this.getAddress() + " " + this.getPhoneNumber() + " " + this.getEmail();
     }
 }
