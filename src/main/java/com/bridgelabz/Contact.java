2e755f76b4c0202f50475fe73c9326834ea09fcd
datta1808 <bagaledatta18@gmail.com>
2021-06-03 13:15:17 +0530

    ADDED: Functionality to View Conatct by City

--- src/main/java/com/bridgelabz/Contact.java
+++ src/main/java/com/bridgelabz/Contact.java
@@ -11,12 +11,11 @@ public class Contact {
     private String phoneNumber;
     private String email;
 
-    public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+    public Contact(String firstName, String lastName, String address, String city, String zip, String phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.city = city;
-        this.state = state;
         this.zip = zip;
         this.phoneNumber = phoneNumber;
         this.email = email;
